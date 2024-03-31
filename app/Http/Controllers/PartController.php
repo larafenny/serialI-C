95e7347ea79b1fd03cd2258f143893ba1640bd79
Lara Filippone <lara.filippone@gmail.com>
2024-03-31 11:34:24 +0200

    Add route /part/{part_serial_number}/remove-part-from-device Add removePartFromDevice in PartController

--- app/Http/Controllers/PartController.php
+++ app/Http/Controllers/PartController.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Part;
+
 class PartController extends Controller
 {
+    public function removePartFromDevice($partSerialNumber)
+    {
+        try {
+            $part = Part::where('Serial_Number', $partSerialNumber)->firstOrFail();
+            $part->Device_SN = null;
+            $part->save();
 
+            return response()->json(['messagge' => 'Part removed successfully from device'], 201);
+        } catch (Exception $e) {
+            throw new Exception('Error removing part from device. Part serial number: ' . $part->Serial_Number .
+                ' Error: ' . $e->getMessage());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\PartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/device/create', [DeviceController::class, 'createNewDevice']);
 Route::post('/device/{device_serial_number}/add-part', [DeviceController::class, 'addPartToDevice']);
+Route::delete('/part/{part_serial_number}/remove-part-from-device', [PartController::class, 'removePartFromDevice']);
